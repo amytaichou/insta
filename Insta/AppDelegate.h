c930f55fe7da5103433ac9a46152d00aa3866fd1
Amy Liu <maymayliu@berkeley.edu>
2018-07-09 14:29:02 -0700

    first commit

---
+++ Insta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Insta
+//
+//  Created by Amy Liu on 7/9/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
